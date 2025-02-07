f8b6aa96299c4dca48dcbc6ce00ba6e21ea1908d
baehl <baehl44@yahoo.fr>
2025-02-07 10:42:10 +0100

    bouton reset ajoute et il fonctionne

--- exo_indiv.php
+++ exo_indiv.php
@@ -1,66 +1,43 @@
-<!-- etape#1: methode $_GET-->
-
-<!-- <!DOCTYPE html>
-<html> 
-<?php
-if (isset($_GET['first_name'])) {
-   echo "Bonjour " . $_GET['first_name'];
-} else {
-   echo "Bonjour anonyme";
-}
-?>
-</html> -->
-
-<!-- etape#2 methode $_POST-->
-<!-- 
-<html> 
-<form action="exo_indiv.php" method="post">
- <p>Votre nom : <input type="text" name="first_name" /></p>
- <p><input type="submit" value="OK"></p>
-</form>
-<?php
-if (isset($_GET['first_name'])) {
-   $prenom = $_GET['first_name'];
-} elseif (isset($_POST['first_name'])) {
-   $prenom = $_POST['first_name'];
-} else {
-   $prenom = 'anonyme';
-}
-echo "Bonjour $prenom";
-?>
-
-</html> -->
-<!-- etape#3 $_SESSION-->
+<!-- etape#4 bouton Reset pour vider la session-->
 <?php
 session_start();
+
+// Si Reset est cliqué, on vide la session
+if(isset($_POST['reset'])) {
+    session_unset();
+    $prenom = 'anonyme';  // On à anonyme après reset
+} 
+
+else {
+    if (isset($_GET['first_name'])) {
+        $prenom = $_GET['first_name'];
+    }
+    elseif (isset($_SESSION['first_name'])) {
+        $prenom = $_SESSION['first_name'];
+    }
+    elseif (isset($_POST['first_name']) && !empty($_POST['first_name'])) {
+        $prenom = $_POST['first_name'];
+        $_SESSION['first_name'] = $prenom;
+    }
+    else {
+        $prenom = 'anonyme';
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
-<form method="post">
-   <p>Votre prenom : <input type="text" name="first_name" /></p>
-   <p><input type="submit" value="OK"></p>
-</form>
-<?php
-  //vérifier l'URL (GET)
-  if (isset($_GET['first_name'])) {
-   $prenom = $_GET['first_name'];
-   // On ne sauvegarde pas dans la session car c'est prioritaire
-}
-//Vérifier la SESSION
-elseif (isset($_SESSION['first_name'])) {
-   $prenom = $_SESSION['first_name'];
-}
-//Vérifier le POST et sauvegarder dans la SESSION
-elseif (isset($_POST['first_name'])) {
-   $prenom = $_POST['first_name'];
-   $_SESSION['first_name'] = $prenom; // Sauvegarde dans la session
-}
-//Sinon, utiliser 'anonyme'
-else {
-   $prenom = 'anonyme';
-}
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <form method="post">
+        <p>Votre prenom : <input type="text" name="first_name" /></p>
+        <p>
+            <input type="submit" value="OK">
+            <input type="submit" name="reset" value="Reset">
+        </p>
+    </form>
 
-echo "Bonjour $prenom";
-?>
-
+    <?php echo "Bonjour $prenom"; ?>
+</body>
 </html>
